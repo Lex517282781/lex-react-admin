5b8137948da3c591dc749020a75038a9e37071e0
517282781 <517282781@qq.com>
2019-10-19 18:34:14 +0800

    pages//form/basic update

--- src/locales/zh-CN/index.js
+++ src/locales/zh-CN/index.js
@@ -1,10 +1,22 @@
 import menu from './common/menu';
 import analysis from './custom/analysis';
 import monitor from './custom/monitor';
+import form from './custom/form';
+import login from './custom/login';
 
 export default {
   'Ant Design Pro': 'Ant Design Pro',
+  'navBar.lang': '语言',
+  'layout.user.link.help': '帮助',
+  'layout.user.link.privacy': '隐私',
+  'layout.user.link.terms': '条款',
+  'app.home.introduce': '介绍',
+  'app.forms.basic.title': '基础表单',
+  'app.forms.basic.description':
+    '表单页用于向用户收集或验证信息，基础表单常见于数据项较少的表单场景。',
   ...menu,
   ...analysis,
-  ...monitor
+  ...monitor,
+  ...form,
+  ...login
 };
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,11 +6,15 @@ import { reducer as dashboardanalysisReducer } from '@/pages/DashboardAnalysis/s
 import { reducer as dashboardmonitorReducer } from '@/pages/DashboardMonitor/store';
 import { reducer as dashboardworkplaceReducer } from '@/pages/DashboardWorkplace/store';
 
+import { reducer as formbasicReducer } from '@/pages/FormBasic/store';
+
 const reducer = combineReducers({
   common: commonReducer,
   dashboardanalysis: dashboardanalysisReducer,
   dashboardmonitor: dashboardmonitorReducer,
-  dashboardworkplace: dashboardworkplaceReducer
+  dashboardworkplace: dashboardworkplaceReducer,
+
+  formbasic: formbasicReducer
 });
 
 export default reducer;
