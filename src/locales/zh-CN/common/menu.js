e1c4f2296966a8e89cc5b9ea5ddf2c8d0ea6c3ec
517282781 <517282781@qq.com>
2019-10-22 21:59:08 +0800

    userlayout update

--- src/locales/zh-CN/common/menu.js
+++ src/locales/zh-CN/common/menu.js
@@ -1,7 +1,8 @@
 export default {
   'menu.home': '首页',
-  'menu.login': '登录',
-  'menu.register': '注册',
+  'menu.user': '用户操作',
+  'menu.user.login': '登录',
+  'menu.user.register': '注册',
   'menu.register.result': '注册结果',
   'menu.dashboard': 'Dashboard',
   'menu.dashboard.analysis': '分析页',
