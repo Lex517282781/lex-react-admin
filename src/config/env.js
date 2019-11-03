12ec98eab4ffb5652255a7308aee11054266a9ad
517282781 <517282781@qq.com>
2019-11-03 23:57:48 +0800

    update

--- src/config/env.js
+++ src/config/env.js
@@ -1,10 +1,10 @@
-const BASEURL = 'http://localhost:9093';
+const BASEURL = 'http://localhost:9093'; // 接口域名统一前缀
 
-const STOREKEY = 'LEX_REACT_ADMIN';
+const STOREKEY = 'LEX_REACT_ADMIN'; // 本地储存KEY
 
-const LOGIN_SIGN_TIMEOUT = 'LOGIN_SIGN_TIMEOUT';
-const LOGIN_SIGN_OK = 'OK';
-const LOGIN_SIGN_ERROR = 'ERROR';
+const LOGIN_SIGN_TIMEOUT = 'LOGIN_SIGN_TIMEOUT'; // 接口返回CODE: 登录过期
+const LOGIN_SIGN_OK = 'OK'; // 接口返回CODE: 登录OK
+const LOGIN_SIGN_ERROR = 'ERROR'; // 接口返回CODE: 登录ERROR
 
 const AUTHORITYOPEN = false;
 
