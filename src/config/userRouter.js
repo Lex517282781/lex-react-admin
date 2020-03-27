d9f674c95fbe31b303187b8a7ed13b4fb2d14332
biansaijun <biansaijun@hzxituan.com>
2020-03-27 22:34:43 +0800

    update

--- src/config/userRouter.js
+++ src/config/userRouter.js
@@ -6,16 +6,16 @@ const router = [
     children: [
       {
         key: 'login',
-        component: asyncComponent(() => import('@/pages/User/Login'))
+        component: asyncComponent(() => import('@/pages/user/Login'))
       },
       {
         key: 'register',
-        component: asyncComponent(() => import('@/pages/User/Register'))
+        component: asyncComponent(() => import('@/pages/user/Register'))
       },
       {
         key: 'register-result',
         component: asyncComponent(() =>
-          import('@/pages/User/Register/subs/RegisterResult')
+          import('@/pages/user/Register/subs/RegisterResult')
         )
       }
     ]
